cff3dc10e9e38cbd8444e8cee247380aa9df365e
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-09-14 17:30:48 -0300

    Initial Commit

---
+++ UberDesign/UberDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberDesignApp.swift
+//  UberDesign
+//
+//  Created by leonardo Moreira on 14/09/23.
+//
+
+import SwiftUI
+
+@main
+struct UberDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
